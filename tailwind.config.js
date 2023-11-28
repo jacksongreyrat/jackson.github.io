046bc392e7a44f5e9ade18b23c876111e74a4aee
JACK2K <asprimail007@gmail.com>
2023-11-28 15:13:57 +0700

    minor adjustment for webfolio v1.0 to 2023 ver.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './index.html'
+  ],
   theme: {
     extend: {
       colors: {
         'body': '#17171F',
         'selected-text': '#A3A3FF',
-        'theme': '#3F3FFF',
-        'nav': '#404053',
+        'theme': '#FAF9F6',
+        'nav': '#FAF9F6',
         'secondary': '#9191A4',
         'badge': '#3F3F51',
         'input-border': '#565666',
-        'input': '#2A2A35'
+        'input': '#2A2A35',
+        'purple: #3f3fff'
       },
       fontFamily: {
         'poppins': ["'Poppins'", 'sans-serif']
       },
       backgroundImage: {
-        'hero-pattern': "url('/img/jackson-pic.JPG')",
-        'footer-texture': "url('/img/jackson-pic.JPG')",
+        'hero-pattern': "url('/img/ethShape')",
+        'footer-texture': "url('/img/ethShape')",
       }
     },
   },
